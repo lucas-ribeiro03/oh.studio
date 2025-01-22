e11168f6ed6cd3e6905c3528c3061dc587b45c07
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-01-22 15:42:04 -0300

    white-background-according-to-url

--- script.js
+++ script.js
@@ -6,6 +6,19 @@ document.addEventListener("DOMContentLoaded", () => {
     once: true,
   });
 
+  const navItems = document.querySelectorAll("nav a");
+  const url = window.location.pathname;
+  const urlSliced = url.slice(0, -5);
+  const urlItem = urlSliced.slice(1);
+  console.log(urlItem);
+
+  navItems.forEach((item) => {
+    const itemContent = item.textContent;
+    if (urlItem === itemContent.toLowerCase()) {
+      item.classList.add(`${itemContent.toLowerCase()}-active`);
+    }
+  });
+
   window.addEventListener("scroll", () => {
     if (scrollY > 135) {
       nav.classList.add("active");
