0ef5dce2254d2eeba1d3dcf6548cec286b1a0c5c
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-01-21 21:52:20 -0300

    blurred-glass-nav

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+const nav = document.querySelector(".navbar");
+
 document.addEventListener("DOMContentLoaded", () => {
   AOS.init({
     duration: 1000,
     once: true,
+  });
+
+  window.addEventListener("scroll", () => {
+    if (scrollY > 135) {
+      nav.classList.add("active");
+    } else {
+      nav.classList.remove("active");
+    }
   });
 });
